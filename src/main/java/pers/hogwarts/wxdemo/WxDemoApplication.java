af5a625587d7f5f219de500a7ad9f49c13977d5f
linxiuke <linxiuke@outlook.com>
2018-11-03 22:41:13 +0800

    init

---
+++ src/main/java/pers/hogwarts/wxdemo/WxDemoApplication.java
@@ -0,0 +1,12 @@
+package pers.hogwarts.wxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxDemoApplication.class, args);
+	}
+}
